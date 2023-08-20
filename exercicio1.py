f073409b29720aef8685efde6754cebab0ee54c7
fnandomachado <fernando.santos@sou.inteli.edu.br>
2023-08-20 12:33:08 -0300

    Exercícios

--- exercicio1.py
+++ exercicio1.py
@@ -1,25 +1,16 @@
 #  Se achar necessario, faça import de outras bibliotecas
 
-
-
-
-
 # Crie a função que será avaliada no exercício aqui
 
+def conta_palavras_unicas(palavras):
+    palavras_unicas = set(palavras.split())
+    print(palavras_unicas)
 
-
-
-
-
+    qntdd = len(palavras_unicas)
+    print(qntdd)
+    return qntdd
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
 
 
 
